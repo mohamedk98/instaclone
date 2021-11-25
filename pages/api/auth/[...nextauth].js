10d1c0d6aa2d102c77c02aed23cf1f869e9741a0
mohamedk98 <73196917+mohamedk98@users.noreply.github.com>
2021-11-25 16:03:40 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,6 +13,7 @@ export default NextAuth({
   pages:{
       signIn:'/auth/signin'
   },
+  secret:'mohamedKhaled',
   callbacks:{
     async session ({session,token,user}) {
       session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase();
